caf7da7a7013823723fab28e0d70022156a83f22
lllytep <Anton199515@yandex.ru>
2014-11-11 12:21:22 +0300

    commit

---
+++ app/src/main/java/ru/ifmo/md/colloquium2/ElectionActivity.java
@@ -0,0 +1,7 @@
+package ru.ifmo.md.colloquium2;
+
+/**
+ * Created by anton on 11.11.14.
+ */
+public class ElectionActivity {
+}
--- app/src/main/java/ru/ifmo/md/colloquium2/MainActivity.java
+++ app/src/main/java/ru/ifmo/md/colloquium2/MainActivity.java
@@ -1,39 +1,76 @@
 package ru.ifmo.md.colloquium2;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    TextView textView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        final Button addButton = (Button) findViewById(R.id.button);
+        final Button editButton = (Button) findViewById(R.id.button2);
+        final Button goButton = (Button) findViewById(R.id.button3);
+
+        final EditText editText = (EditText)findViewById(R.id.editText);
+
+        final ListView listView = (ListView)findViewById(R.id.listView);
+
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+
+        listView.setAdapter(adapter);
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String candidate = editText.getText().toString();
+                if(candidate.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Error! EditText is empty!", Toast.LENGTH_LONG);
+                } else{
+                    adapter.add(candidate);
+                }
+            }
+        });
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String candidate = editText.getText().toString();
+                if(!candidate.isEmpty())
+                    textView.setText(candidate);
+                else
+                    Toast.makeText(getApplicationContext(), "Error! EditText is empty!", Toast.LENGTH_LONG);
+            }
+        });
 
-        return super.onOptionsItemSelected(item);
+        goButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent();
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                textView = (TextView)view;
+            }
+        });
+
     }
 }
